b19cc6dbfd025dabc01adc30fb2731bc9fb6cc23
Anweasha Saha <anweashasaha2000@gmail.com>
2025-07-27 21:41:08 -0700

    Delete easy directory

--- easy/Q1_Histogram_of_Tweets.sql
+++
@@ -1,8 +0,0 @@
-SELECT num_tweets AS tweet_bucket, COUNT(*) AS users_num
-FROM (
-SELECT user_id, COUNT(*) AS num_tweets
-FROM tweets
-WHERE EXTRACT(YEAR FROM tweet_date) = 2022
-GROUP BY user_id
-) AS X
-GROUP BY num_tweets
