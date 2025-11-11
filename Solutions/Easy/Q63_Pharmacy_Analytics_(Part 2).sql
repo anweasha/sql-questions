e901148b8df3f056b9ea34a935ef4448a374b9c3
Anweasha Saha <anweashasaha2000@gmail.com>
2025-11-11 12:16:30 -0800

    Create Q63_Pharmacy_Analytics_(Part 2).sql

---
+++ Solutions/Easy/Q63_Pharmacy_Analytics_(Part 2).sql
@@ -0,0 +1,6 @@
+SELECT manufacturer, COUNT(drug) AS drug_count, 
+SUM(cogs - total_sales) AS total_loss
+FROM pharmacy_sales
+WHERE total_sales < cogs
+GROUP BY manufacturer
+ORDER BY total_loss DESC
