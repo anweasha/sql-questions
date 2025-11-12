f6464ed4b25fa89546f8c6468070372ee3dbb533
Anweasha Saha <anweashasaha2000@gmail.com>
2025-11-12 10:29:49 -0800

    Create Q69_Patient_Support_Analysis_(Part 1).sql

---
+++ Solutions/Easy/Q69_Patient_Support_Analysis_(Part 1).sql
@@ -0,0 +1,8 @@
+WITH Temp AS (
+  SELECT policy_holder_id
+  FROM callers
+  GROUP BY policy_holder_id
+  HAVING COUNT(case_id) >= 3
+)
+SELECT COUNT(policy_holder_id) AS policy_holder_count
+FROM Temp
