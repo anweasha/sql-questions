728c8623d1c6801c21a0dd87ac8007028fbef899
Anweasha Saha <anweashasaha2000@gmail.com>
2025-11-21 13:10:59 -0800

    Create Q160_Employees_With_Missing_Information.sql

---
+++ Solutions/Easy/Q160_Employees_With_Missing_Information.sql
@@ -0,0 +1,4 @@
+SELECT COALESCE(e.employee_id, s.employee_id) AS employee_id
+FROM Employees AS e
+FULL OUTER JOIN Salaries AS s ON e.employee_id = s.employee_id
+WHERE e.name IS NULL OR s.salary IS NULL
