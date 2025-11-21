6ea8ee71eacd66306a21ac83d303644ca1717579
Anweasha Saha <anweashasaha2000@gmail.com>
2025-11-21 13:12:47 -0800

    Create Q163_DNA_Pattern_Recognition.sql

---
+++ Solutions/Medium/Q163_DNA_Pattern_Recognition.sql
@@ -0,0 +1,6 @@
+SELECT *,
+CASE WHEN dna_sequence REGEXP '^ATG' THEN 1 ELSE 0 END AS has_start,
+CASE WHEN dna_sequence REGEXP '(TAA|TAG|TGA)$' THEN 1 ELSE 0 END AS has_stop,
+CASE WHEN dna_sequence REGEXP 'ATAT' THEN 1 ELSE 0 END AS has_atat,
+CASE WHEN dna_sequence REGEXP 'G{3,}' THEN 1 ELSE 0 END AS has_ggg
+FROM Samples
